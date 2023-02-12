f9e8ee392ac43b6e2f13a91c51b00a5f83ba4750
Roman <rmn.polishchuk@gmail.com>
2023-02-12 22:49:42 +0300

    Update api routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,8 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +11,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource("users", \App\Http\Controllers\UserController::class);
+Route::apiResource("cities", \App\Http\Controllers\CityController::class, ['except' => ['create', 'edit', 'remove']]);
+Route::apiResource("statuses", \App\Http\Controllers\MaritalStatusController::class, ['except' => ['create', 'edit', 'remove']]);
+Route::apiResource("citizenships", \App\Http\Controllers\CitizenshipController::class, ['except' => ['create', 'edit', 'remove']]);
+Route::apiResource("disabilities", \App\Http\Controllers\DisabilityController::class, ['except' => ['create', 'edit', 'remove']]);
