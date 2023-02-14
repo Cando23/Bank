d349102c095bf95c865b6f13351e6ab31de8acc6
Roman <rmn.polishchuk@gmail.com>
2023-02-14 11:32:06 +0300

    Refactor routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\CitizenshipController;
+use App\Http\Controllers\CityController;
+use App\Http\Controllers\DisabilityController;
+use App\Http\Controllers\MaritalStatusController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
@@ -11,8 +16,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::apiResource("users", \App\Http\Controllers\UserController::class);
-Route::apiResource("cities", \App\Http\Controllers\CityController::class, ['except' => ['create', 'edit', 'remove']]);
-Route::apiResource("statuses", \App\Http\Controllers\MaritalStatusController::class, ['except' => ['create', 'edit', 'remove']]);
-Route::apiResource("citizenships", \App\Http\Controllers\CitizenshipController::class, ['except' => ['create', 'edit', 'remove']]);
-Route::apiResource("disabilities", \App\Http\Controllers\DisabilityController::class, ['except' => ['create', 'edit', 'remove']]);
+Route::apiResource("users", UserController::class);
+Route::get("cities", [CityController::class, "index"]);
+Route::get("cities/{city}", [CityController::class, "show"]);
+Route::get("statuses", [MaritalStatusController::class, "index"]);
+Route::get("statuses/{status}", [MaritalStatusController::class, "show"]);
+Route::get("citizenships", [CitizenshipController::class, "index"]);
+Route::get("citizenships/{citizenship}", [CitizenshipController::class, "show"]);
+Route::get("disabilities", [DisabilityController::class, "index"]);
+Route::get("disabilities/{disability}", [DisabilityController::class, "show"]);
